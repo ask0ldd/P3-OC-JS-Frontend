3c5d7fc61524e01ef45840a721ea2feb2533de9d
Cedric B-Eff <cedrikopenclass@gmx.com>
2022-11-13 21:08:58 +0100

    Gallery with filters / filters non functional yet

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,5 +1,6 @@
 const api = 'http://localhost:5678/api/'
-const gallery = document.querySelector(".gallery")
+const galleryContainer = document.querySelector(".gallery")
+const filtersContainer = document.querySelector(".filters")
 
 /*
 <figure>
@@ -7,21 +8,45 @@ const gallery = document.querySelector(".gallery")
     <figcaption>Abajour Tahina</figcaption>
 </figure>*/
 
+function emptyGallery()
+{
+    galleryContainer.innerHTML=""
+}
+
+//all works
 fetch(`${api}works`).then((response)=>{
     const readReponse = response.json()
     return readReponse
 }).then((data) => {
+
     console.log(data)
+
+    let categories = []
+    let pushedIds = []
+
     for(let i=0; i<Object.keys(data).length; i++)
     {
         let work = document.createElement("figure");
-        work.innerHTML = `<img src="${data[i].imageUrl}" alt="Abajour Tahina" crossorigin="anonymous"><figcaption>${data[i].title}</figcaption>`
-        gallery.append(work)
+        
+        // rework avec createelement ?
+        work.innerHTML = `<img src="${data[i].imageUrl}" alt="${data[i].title}" crossorigin="anonymous"><figcaption>${data[i].title}</figcaption>`
+        galleryContainer.append(work)
+
+        // getfilters
+
+        if(pushedIds.includes(data[i].category.id) === false)
+        {
+            categories.push(data[i].category)
+            pushedIds.push(data[i].category.id)
+        }
     }
 
+    categories.forEach(element => { 
+        let button = document.createElement("div")
+        button.textContent = element.name
+        button.addEventListener("click", () => console.log(element.id))
+        filtersContainer.append(button)
+    })
 })
 
-function emptyGallery()
-{
-    gallery.innerHTML=""
-}
+
