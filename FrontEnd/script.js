8a4a2c7cc5bdb3c2bb4b86f551ffffa13b37aa9f
Cedric B-Eff <cedrikopenclass@gmx.com>
2022-11-14 22:38:29 +0100

    Modale gallery visible

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -3,18 +3,18 @@ const galleryContainer = document.querySelector(".gallery")
 const filtersContainer = document.querySelector(".filters")
 const loginButton = document.querySelector("nav > ul > li:nth-child(3)")
 const contactButton = document.querySelector("nav > ul > li:nth-child(2)")
+const editIcon = document.querySelectorAll(".edit__icon")
+const editTopBar = document.querySelector(".editionmode__topbar")
+const header = document.querySelector("#header")
+const editModeContainer = document.querySelector("#opaque__container")
+const editGallery = document.querySelector("#edition__gallery")
 
 // delete final upload
-const email = "sophie.bluel@test.tld"
-const password = "S0phie"
-const wrongEmail = "ezaeaz.ezaeza@test.tld"
-const wrongPassword = "ezaeza"
+const user = {'email':'sophie.bluel@test.tld','password':'S0phie'}
+const unauthorizedUser = {'email':'ezaeaz.ezaeza@test.tld','password':'ezaeza'}
 
-/*
-<figure>
-    <img src="assets/images/abajour-tahina.png" alt="Abajour Tahina">
-    <figcaption>Abajour Tahina</figcaption>
-</figure>*/
+// creer class pr gallery edit avec ts les elements
+
 
 function emptyGallery()
 {
@@ -26,16 +26,85 @@ function emptyFilters()
     filtersContainer.innerHTML=""
 }
 
-function tokenExists()
+function tokenAlive()
 {
-    let cookie = document.cookie
-    //console.log(typeof(cookie))
+    //gerer si pas de cookie ou cookie pas string
+    const cookie = document.cookie
+    /*console.log(typeof(cookie))
+    console.log(cookie)*/
     return cookie.search("token")===-1 ? false : true
 }
 
-function getUniqueCategories(arr)
+function getUniqueCategories(obj)
 {
     let pushedIds = []
+    let categories = []
+
+    for(let i=0; i<Object.keys(obj).length; i++)
+    {
+        // try get same result with set
+        // push a category {id, name} only if the current id hasn't been pushed yet
+        if(pushedIds.includes(obj[i].category.id) === false)
+        {
+            categories.push(obj[i].category)
+            pushedIds.push(obj[i].category.id)
+        }
+    }
+
+    return categories
+}
+
+function getWorks(obj)
+{
+    let works = []
+
+    for(let i=0; i<Object.keys(obj).length; i++)
+    {
+        works.push({
+            'id' : obj[i].id, 
+            'title' : obj[i].title, 
+            'url' : obj[i].imageUrl, 
+            'category' : obj[i].categoryId
+            })
+    }
+
+    console.log(works)
+    return works
+
+}
+
+async function populateEditGallery()
+{
+    await fetch(`${api}works`).then((response)=>{
+        return response.json()
+    }).then((data) => {
+
+        // reset gallery
+        editGallery.innerHTML=""
+
+        // get works as an array of objects of data
+        const works = getWorks(data)
+
+
+        works.forEach(work => 
+        {
+            let div = document.createElement("div")
+            let img = document.createElement("img")
+            let bin_icon = document.createElement("img")
+            div.style.position="relative"
+            img.crossOrigin="anonymous"
+            img.src=work.url
+            img.classList.add("work__img")
+            bin_icon.src = "./assets/icons/bin_icon.png"
+            bin_icon.classList.add("bin__icon")
+            div.append(img)
+            div.append(bin_icon)
+            editGallery.append(div)
+        })
+
+    }).catch(error => {
+        console.log(error)
+    })
 }
 
 // !!! deal with errors
@@ -55,6 +124,9 @@ async function filterWork(selectedCategoryId)
 
         emptyGallery()
         emptyFilters()
+        
+        console.log(data)
+        console.log(typeof(data))
     
         for(let i=0; i<Object.keys(data).length; i++)
         {
@@ -63,6 +135,7 @@ async function filterWork(selectedCategoryId)
             // select works where id === selectedCategoryId or all works if selectedCategoryId === 0
             if((data[i].category.id === selectedCategoryId)||(selectedCategoryId === 0)) {
                 work.innerHTML = `<img src="${data[i].imageUrl}" alt="${data[i].title}" crossorigin="anonymous"><figcaption>${data[i].title}</figcaption>` // CORS
+                //verifier que gallerycontainer exist
                 galleryContainer.append(work)
             }
     
@@ -122,11 +195,17 @@ async function log(login, password)
 
 }
 
+function showEditButtonsonIndex(){
+    editIcon.forEach(el => 
+        {
+            el.classList.toggle('edit__icon--on')
+            //el.addEventListener('click', populateEditGallery())
+        })
+    editTopBar.classList.toggle('editionmode__topbar--on')
+    header.classList.toggle('header__padding--notopBar')
+}
+
 //MAIN
-
-//filterWork(0) // 0 = All
-
-let user
 
 /*loginButton.addEventListener("click", () => log(email, password).then((userDatas) => {
     user = userDatas
@@ -142,12 +221,33 @@ contactButton.addEventListener("click", () => {
 
 function tryLog ()
 {
-    console.log('trying to log sir')
+    //console.log('trying to log sir')
     log(email, password).then((userDatas) => 
     {
         //if(user === undefined){}
         document.cookie = `id=${userDatas.userId}; Secure`;
         document.cookie = `token=${userDatas.token}; Secure`;
-        //window.location.href = "index.html"
+        window.location.href = "index.html"
     })
+}
+
+function scrollLock(){
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    window.onscroll = function() {
+        window.scrollTo(scrollLeft, scrollTop);
+    };
+}
+
+function switchToEditMode(){
+    scrollLock()
+    //editModeContainer.classList.toggle('opaque__container--visible')
+    editModeContainer.style.display="flex"
+    populateEditGallery()
+
+}
+
+function indexOnLoad(){
+    filterWork(0)
+    tokenAlive() ? showEditButtonsonIndex() : false
 }
